757aa1ab4121e3cd4d099c89ef9e895f76dcc5c0
Karen Coombs <librarywebchic@gmail.com>
2014-05-16 11:24:05 -0500

    Made search form send GET instead of POST

--- app/views/search.blade.php
+++ app/views/search.blade.php
@@ -5,7 +5,7 @@
         <div style="margin: 10px 10px; font-size: 2em;">My Library</div>
     </div>    
     <div id="search-wrapper" class="span-24">
-        {{ Form::open(array('url' => 'search', 'id' => 'search-form')) }}
+        {{ Form::open(array('url' => 'search', 'id' => 'search-form', 'method' => 'get')) }}
         {{ Form::label('q', 'Catalog', array('style' => 'font-size: 2em; margin-right:1em;'))}}
         {{ Form::text('q', null, array('size' => 40))}}
         {{ Form::submit('Search')}}
