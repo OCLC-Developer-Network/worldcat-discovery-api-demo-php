67dd6c49d5884129c1b83c92c6726d63eb0cd4df
Karen Coombs <librarywebchic@gmail.com>
2014-08-14 11:28:49 -0500

    Updated Error template to use new WorldCat\Discovery\Error class

--- app/views/error.blade.php
+++ app/views/error.blade.php
@@ -1,11 +1,6 @@
 @extends('layouts.master')
 
 @section('content')
-<p>Status Code - {{$error->getCode()}}</p>
-<p>Error Message - {{$$error->getMessage()}}</p>
-<h4>Request</h4>
-<pre>
-{{$error->getRequest()}}
-
-</pre>
+<p>Status Code - {{$error->getErrorCode()}}</p>
+<p>Error Message - {{$error->getErrorMessage()}}</p>
 @stop
