66dcd628554257fcbad625b089e2f8815764f6ae
Karen Coombs <librarywebchic@gmail.com>
2014-09-08 15:33:49 -0500

    Changed formating for offers

--- app/views/offers.blade.php
+++ app/views/offers.blade.php
@@ -1,13 +1,17 @@
-<div id="offers" class="span-6 last">
-@if ($offers || getFulltextLink($record))
-    <h2>Holdings</h2>
-    <ul>
-    @foreach ($offers as $offer)
-        <li>{{$offer->getSeller()->getName()}}}}</li>
-    @endforeach
-    @if (getFulltextLink($record))
-        <li>{{link_to(getFulltextLink($record), 'Get Electronic Copy')}}</li>
+<div id="availability-and-more" class="span-10 last">
+    <div id="availability" class="span-10">
+    <h2>Availability</h2>
+    @if ($offers || getFulltextLink($record))
+        @foreach ($offers as $offer)
+            <p>{{$offer->getSeller()->getName()}}</p>
+        @endforeach
+        @if (getFulltextLink($record))
+            <p>{{link_to(getFulltextLink($record), 'Get Electronic Copy')}}</p>
+        @endif
+    @else
+        <p>This item is not available at your library</p>
     @endif
-    </ul>
-@endif
+    </div>
+    <div id="more-info" class="span-10 last"/>
+    
 </div>
