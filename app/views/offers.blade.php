d74b983a443266b728948ee9611fce6a26f9e090
Karen A. Coombs <librarywebchic@gmail.com>
2014-12-02 13:27:22 -0600

    Fixed issues with EHoldings showing when not enabled

--- app/views/offers.blade.php
+++ app/views/offers.blade.php
@@ -1,7 +1,7 @@
 
     <div id="availability" class="span-10">
     <h2>Availability</h2>
-    @if ($offers || getFulltextLink($record))
+    @if ($offers || (Config::get('app.showEHoldings') && getFulltextLink($record)))
         @foreach ($offers as $offer)
             <p>{{$offer->getSeller()->getName()}}</p>
             @if (Config::get('app.showAvailability') && getAvailability($record->getOCLCNumber(), $offer->getSeller()))
